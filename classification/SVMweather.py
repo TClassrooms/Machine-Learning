00aa3bc4b991948c8fdbd44e6cca700a9517113c
VictorCarreira <victorcarreira@on.br>
2019-11-25 16:34:38 -0300

    Coloca a função que plota o gráfico da função confusão no module/iplots.py e adiciona o método de inclusão de modules

--- classification/SVMweather.py
+++ classification/SVMweather.py
@@ -1,6 +1,14 @@
+###################################################################################
+######################### A SUPPORT VECTOR MACHINE PROGRAM ########################
+###################################################################################
+
+#Importando pacotes
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0,'modules/')
+import iplots as mfun 
 
 
 
@@ -62,6 +70,14 @@ svclassifier.fit(X_train, y_train)
 
 
 
+###################################################################################
+#-------------------------------- CLASSIFICAÇÃO ----------------------------------#
+###################################################################################
+
+# Nesta etapa, classifica o conjunto de entrada do dado a ser classificado com base 
+#no modelo de SVM já treinado na fase anterior. O resultado da classificação é feita
+#com base na matriz de confusão. 
+
 
 y_pred = svclassifier.predict(X_test)
 
@@ -74,77 +90,16 @@ print(classification_report(y_test,y_pred))
 
 
 
-
-
-
-
-
-
-
-
-#contains 
-def plot_confusion_matrix(y_true, y_pred, classes,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
-
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-    # Only use the labels that appear in the data
-    classes = classes[unique_labels(y_true, y_pred)]
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=classes, yticklabels=classes,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    fig.tight_layout()
-    return ax
-
-
-np.set_printoptions(precision=2)
+# Visualização gráfica dos resultados:
 
 # Plot non-normalized confusion matrix
-plot_confusion_matrix(y_test, y_pred, classes=y,
-                      title='Confusion matrix, without normalization')
+mfun.plot_confusion_matrix(y_test, y_pred, classes=y,title='Confusion matrix, without normalization')
+
+plt.show()
 
 # Plot normalized confusion matrix
-plot_confusion_matrix(y_test, y_pred, classes=y, normalize=True,
-                      title='Normalized confusion matrix')
+mfun.plot_confusion_matrix(y_test, y_pred, classes=y, normalize=True,title='Normalized confusion matrix')
+
+plt.show()
 
 
